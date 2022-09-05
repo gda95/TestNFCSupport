ce945e835e91d31760838e9aebb089a5fd11cffa
gda2 <guillaume_dabosville@yahoo.fr>
2022-09-05 17:42:14 +0200

    First commit

---
+++ app/src/main/java/com/example/testnfcsupport/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.testnfcsupport
+
+import android.content.pm.PackageManager.FEATURE_NFC_HOST_CARD_EMULATION
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.testnfcsupport.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.switchNfcSupport.setOnClickListener{ testNFCSupport() }
+    }
+
+    private fun testNFCSupport() {
+        val pm = packageManager
+        val b = pm.hasSystemFeature(FEATURE_NFC_HOST_CARD_EMULATION)
+        if (binding.switchNfcSupport.isChecked) {
+            if (b)
+                binding.textNfcSupport.text = getString(R.string.not_supported, "Yes")
+            else
+                binding.textNfcSupport.text = getString(R.string.not_supported, "No")
+        }
+    }
+}
